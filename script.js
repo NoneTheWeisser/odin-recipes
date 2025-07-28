6c3b2395bc0644d97994aa0c5b6e103e729ef16f
NoneTheWeisser <214901165+NoneTheWeisser@users.noreply.github.com>
2025-07-28 09:42:00 -0500

    nav bar updated to hamburger menu, added styles to linked pages

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ document.addEventListener("DOMContentLoaded", readyNow);
 function readyNow() {
   console.log("DOM is loaded!");
 }
+
+// Nav bar hamburger
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+// toggle menu on hamburger icon
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+  hamburger.classList.toggle("open");
+
+    // switch icon from = to X
+    if (hamburger.classList.contains("open")){
+    hamburger.innerHTML = "&times"; 
+    } else {
+    hamburger.innerHTML = "&#9776;"
+    }
+});
+
+// close menu when link is clicked
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("show");
+        hamburger.classList.remove("open");
+        hamburger.innerHTML = "&#9776";
+    });
+});
+
